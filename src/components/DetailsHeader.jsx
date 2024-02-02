55249eb3809e924091addb15aba1689aafc3e12a
robbie-carragher <orcafinweb@gmail.com>
2024-02-01 21:14:59 -0800

    inital set up of repo

---
+++ src/components/DetailsHeader.jsx
@@ -0,0 +1,34 @@
+import { Link } from "react-router-dom";
+
+const DetailsHeader = ({ artistId, artistData, songData }) => {
+
+<div className="relative w-full flex flex-col">
+
+
+<div className="w-full bg-gradient-to-1 from-transparent to-black sm:h-48 h-28">
+
+<div className="absolute inset-0 flex items-center">
+  {/* <img 
+  
+  alt="art"
+  src={artistId ? artistData?.artists[artistId].attributes?.
+  artwork?.url.replace('{w}', '500').replace('{h}', '500')}
+  
+  
+  
+  /> */}
+</div>
+
+</div>
+
+
+</div>
+
+
+
+
+
+
+ 
+};
+export default DetailsHeader;
---
+++ src/pages/SongDetails.jsx
@@ -0,0 +1,160 @@
+// import React from 'react';
+// import { useParams } from 'react-router-dom';
+// import { useSelector, useDispatch } from 'react-redux';
+// import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
+// import { setActiveSong, playPause } from '../redux/features/playerSlice';
+// import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
+
+// const SongDetails = () => {
+
+//   const dispatch = useDispatch();
+//   const { songid, id: artistId } = useParams();
+//   const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+//   const { data, isFetching: isFetchinRelatedSongs, error } = useGetSongRelatedQuery({ songid });
+//   const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
+
+//   if (isFetchingSongDetails && isFetchinRelatedSongs) return <Loader title="Searching song details" />;
+
+//   console.log(songData);
+//   console.log(data)
+
+//   if (error) return <Error />;
+
+//   const handlePauseClick = () => {
+//     dispatch(playPause(false));
+//   };
+
+//   const handlePlayClick = (song, i) => {
+//     console.log(song);
+//     dispatch(setActiveSong({ song, data, i }));
+//     dispatch(playPause(true));
+//   };
+
+//   return (
+//     <div className="flex flex-col">
+//       <DetailsHeader
+//         artistId={artistId}
+//         songData={songData}
+//       />
+
+//       <div className="mb-10">
+//         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+
+//         <div className="mt-5">
+//           {songData?.sections[1].type === 'LYRICS'
+//             ? songData?.sections[1]?.text.map((line, i) => (
+//               <p key={`lyrics-${line}-${i}`} className="text-gray-400 text-base my-1">{line}</p>
+//             ))
+//             : (
+//               <p className="text-gray-400 text-base my-1">Sorry, No lyrics found!</p>
+//             )}
+//         </div>
+//       </div>
+
+//       <RelatedSongs
+//         data={data}
+//         artistId={artistId}
+//         isPlaying={isPlaying}
+//         activeSong={activeSong}
+//         handlePauseClick={handlePauseClick}
+//         handlePlayClick={handlePlayClick}
+//       />
+
+//     </div>
+//   );
+// };
+
+// export default SongDetails;
+import React from "react";
+import { useParams } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
+
+import { setActiveSong, playPause } from "../redux/features/playerSlice";
+import {
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} from "../redux/services/shazamCore";
+
+const SongDetails = () => {
+  const dispatch = useDispatch();
+  const { songid } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+
+  const {
+    data: relatedSongsData,
+    isFetching: isFetchingRelatedSongs,
+    error: relatedSongsError,
+  } = useGetSongRelatedQuery({ songid });
+
+  const {
+    data: songData,
+    isFetching: isFetchingSongDetails,
+    error: songDetailsError,
+  } = useGetSongDetailsQuery({ songid });
+
+  // Handle loading state
+  if (isFetchingSongDetails || isFetchingRelatedSongs) {
+    return <Loader title="Searching song details" />;
+  }
+
+  // Handle errors
+  if (songDetailsError || relatedSongsError) {
+    return <Error />;
+  }
+
+  // Extract lyrics if available
+  const lyricsSection = songData?.sections?.find(
+    (section) => section.type === "LYRICS"
+  );
+
+  const handlePauseClick = () => {
+    dispatch(playPause(false));
+  };
+
+  const handlePlayClick = (song, i) => {
+    dispatch(setActiveSong({ song, data: relatedSongsData, i }));
+    dispatch(playPause(true));
+  };
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader
+        artistId={songData?.artists?.[0]?.adamid}
+        songData={songData}
+      />
+
+      <div className="mb-10">
+        <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
+        <div className="mt-5">
+          {lyricsSection ? (
+            lyricsSection.text.map((line, i) => (
+              <p
+                key={`lyrics-${line}-${i}`}
+                className="text-gray-400 text-base my-1"
+              >
+                {line}
+              </p>
+            ))
+          ) : (
+            <p className="text-gray-400 text-base my-1">
+              Sorry, No lyrics found!
+            </p>
+          )}
+        </div>
+      </div>
+
+      <RelatedSongs
+        data={relatedSongsData}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+        handlePauseClick={handlePauseClick}
+        handlePlayClick={handlePlayClick}
+      />
+    </div>
+  );
+};
+
+export default SongDetails;
